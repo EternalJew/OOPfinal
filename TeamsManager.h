492a9a158dd7def79bc053dd6ceaff23d88ddc7c
Oleksiy <58302277+porkbelly-lab@users.noreply.github.com>
2020-01-15 09:55:11 +0200

    Update TeamsManager.h

--- TeamsManager.h
+++ TeamsManager.h
@@ -1,23 +1,72 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <Ctime>
-#include <time.h>
-#include <stdio.h>
-#include "Teams.h"
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 using namespace std;
-int main(int argc, char** argv) {
-	Players name;
-	Players ranks;
-	name.message();
-	name.set("PlayMars","porkbelly","pokemon28","Jupiter","kalash_drobodan","s1mple","Zeus","_1234554321_","3drob14","branch");
-	name.get();
-	ranks.set("silver 1","silver 2","silver elite","silver 2","silver 1","silver 2","silver 3","silver elite","silver 2","silver 3");
-	ranks.get();
-	teams random;
-	random.message();
-	random.message1();
-	return 0;
-}
+
+class PlayerManager
+{
+public:
+	vector<Player> players;
+
+	void generatePlayers(int n) {
+		vector<string> names = {
+			"Tom", "Jame", "Stive", "John", "Rick", "Fred", "Eric", "Diego", "Luke", "Philip", "Oliver", "Jake"
+		};
+		srand((unsigned)time(NULL));
+		for (int i = 0; i < n; i++) {
+			int idx = rand() % names.size();
+			Player player = Player(names[idx], 1000, i + 1);
+			players.push_back(player);
+		}
+	}
+
+	void sortPlayersById() {
+		for (size_t i = 0; i < players.size() - 1; i++)
+		{
+			for (size_t j = 0; j < players.size() - i - 1; j++)
+			{
+				if (players[j].getID() > players[j + 1].getID()) swap(players[j], players[j + 1]);
+			}
+		}
+	}
+
+	void ListOfPlayers() {
+		sortPlayersById();
+		cout << "==================================" << endl << endl;
+		for (size_t i = 0; i < players.size(); i++) {
+			cout << "Name: " << players[i].getName()
+				<< "\tID: " << players[i].getID()
+				<< "\tRank: " << players[i].getRank()
+				<< endl << endl
+				<< "=================================="
+				<< endl << endl;
+		}
+	}
+
+	Player GetPlayerByName(string name) {
+		for (size_t i = 0; i < players.size(); i++) {
+			if (players[i].getName() == name) {
+				return players[i];
+			}
+		}
+	}
+
+	Player GetPlayerById(int id) {
+		for (size_t i = 0; i < players.size(); i++) {
+			if (players[i].getID() == id) {
+				return players[i];
+			}
+		}
+	}
+
+	void AddPlayer(string name) {
+		int id = players[players.size() - 1].getID() + 1;
+		Player player = Player(name, 1000, id);
+		players.push_back(player);
+	}
+
+	void RemovePlayer(int id) {
+		for (size_t i = 0; i < players.size(); i++) {
+			if (players[i].getID() == id) players.erase(players.begin() + i);
+			break;
+		}
+	}
+};
